020e6ebcd3b4f551f62ab28eba42d90f9ecdefea
Clayon Stewart <c.stewart89@gmail.com>
2021-03-12 15:08:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailAddressParser
 
   
   def initialize(email_string)
-    @emails = email_string.split(/[,]?\s/).unique
+    @emails = email_string.split(/[,]?\s/).uniq
   end
   
   def parse
